5adfa6037ee04ae2adc9e65261b0dd9c2f68b2a6
muchirasamwel <muchirasamwel@gmail.com>
2025-01-20 12:09:20 +0300

    * Add simple lottery contract logic

---
+++ contracts/Lottery.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Lottery {
+   address payable [] public players;
+   address public manager;
+
+   constructor (){
+      manager=msg.sender;
+   }
+
+   function joinDraw() external payable{
+      require(msg.value > 1 ether,"You need to send atleast 0.0011 ether");
+      players.push(payable (msg.sender));
+   }
+
+   function random() private view returns (uint) {
+      return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
+   }
+
+   function pickWinner() external restricted {
+      
+      uint index = random() % players.length;
+      players[index].transfer(address(this).balance);
+      players = new address payable [](0);
+   }
+
+   function getPlayers() external view restricted returns (address payable [] memory){
+      return players;
+   }
+
+   modifier restricted(){
+      require(manager==msg.sender, "You are not the manager. Only the manager can trigger this method");
+      _;
+   }
+}

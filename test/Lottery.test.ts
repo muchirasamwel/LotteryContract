4dd31727879a18927fc6c1536d36235bd16a7106
muchirasamwel <muchirasamwel@gmail.com>
2025-01-20 12:57:41 +0300

    * Update test file

--- test/Lottery.test.ts
+++ test/Lottery.test.ts
@@ -1,22 +1,28 @@
-import hre from "hardhat";
+import { ethers } from "hardhat";
 import { expect } from "chai";
 import { Lottery } from "../typechain-types";
 import { ContractTransactionResponse } from "ethers";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("Lottery contract", function () {
+  let owner: HardhatEthersSigner;
+  let account1: HardhatEthersSigner;
+  let account2: HardhatEthersSigner;
+
   let lotteryContract: Lottery & {
     deploymentTransaction(): ContractTransactionResponse;
   };
   this.beforeEach(async () => {
-    const lotteryFactory = await hre.ethers.getContractFactory("Lottery");
+    [owner, account1, account2] = await ethers.getSigners();
+
+    const lotteryFactory = await ethers.getContractFactory("Lottery");
     lotteryContract = await lotteryFactory.deploy();
     await lotteryFactory.getDeployTransaction();
 
     console.log("deployed...", lotteryContract.target);
   });
 
-  it("Manager to be the contrant owner", async () => {
-    const [owner, otherAccount] = await hre.ethers.getSigners();
+  it("Manager is the contrant owner", async () => {
     expect(await lotteryContract.manager()).to.equal(owner);
   });
 });

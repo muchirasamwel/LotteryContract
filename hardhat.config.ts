ab630fac4b7d13e069b4981ad801f35711b0b8c7
muchirasamwel <muchirasamwel@gmail.com>
2025-01-21 15:27:13 +0300

    * Add sepoliaTestNet network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepoliaTestNet: {
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_KEY,
+      accounts: ["" + process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;

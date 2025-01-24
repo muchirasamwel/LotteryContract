b7d0b287a1c31be15aad3f0d11e5a74af9d77736
muchirasamwel <muchirasamwel@gmail.com>
2025-01-24 19:35:10 +0300

    * Add more info to game state (players, prize and manager)

--- lottery-next-frontend/src/components/GameStatus.tsx
+++ lottery-next-frontend/src/components/GameStatus.tsx
@@ -1,16 +1,24 @@
 import React, { useEffect } from "react";
 
-type Props = {};
+type Props = { manager?: string; players?: string[]; prize?: string };
 
-const GameStatus = (props: Props) => {
-  useEffect(() => {}, []);
+const GameStatus = ({ manager, players, prize }: Props) => {
   return (
-    <div>
+    <div className="p-4 border-2 border-gray-200 rounded-lg flex flex-col justify-center items-center">
       <div className="text-xl font-bold">Game Status</div>
+      <div className="text-lg font-bold">Prize</div>
+      <ul className="text-lg font-bold text-green-700">
+        <li>{prize}</li>
+      </ul>
+      <div className="text-lg font-normal">Manager</div>
+      <ul className="text-lg font-normal text-green-700">
+        <li>{manager}</li>
+      </ul>
       <div className="text-lg font-normal">Entries</div>
       <ul className="text-lg font-normal text-green-700">
-        <li>0x1234</li>
-        <li>0x5678</li>
+        {players?.map((player) => (
+          <li key={player}>{player}</li>
+        ))}
       </ul>
     </div>
   );

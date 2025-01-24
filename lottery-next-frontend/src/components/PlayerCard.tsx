10af82f48f02eb308b1cab786fbf4c295b6d78df
muchirasamwel <muchirasamwel@gmail.com>
2025-01-24 19:37:11 +0300

    * Link player actions on player card

--- lottery-next-frontend/src/components/PlayerCard.tsx
+++ lottery-next-frontend/src/components/PlayerCard.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
-type Props = {};
+type Props = {
+  joinDraw: () => Promise<void>;
+  pickWinner: () => Promise<void>;
+};
 
-const PlayerCard = (props: Props) => {
+const PlayerCard = ({ joinDraw, pickWinner }: Props) => {
+  const [joining, setjoining] = useState(false);
+  const [pickingWinner, setpickingWinner] = useState(false);
+  const handleJoinDraw = async () => {
+    console.log("Join Draw");
+    setjoining(true);
+    await joinDraw();
+    setjoining(false);
+    console.log("Join Draw Done");
+  };
+
+  const handlePickWinner = async () => {
+    console.log("Pick Winner");
+    setpickingWinner(true);
+    await pickWinner();
+    setpickingWinner(false);
+    console.log("Pick Winner Done");
+  };
   return (
-    <div>
+    <div className="p-4 border-2 border-gray-200 rounded-lg flex flex-col justify-center items-center">
       <div className="text-xl font-bold">Player Options</div>
-      <Button onClick={() => {}}>Join Draw</Button>
-      <Button className={"bg-red-400"} onClick={() => {}}>
+      <Button loading={joining} onClick={handleJoinDraw}>
+        Join Draw
+      </Button>
+      <Button
+        loading={pickingWinner}
+        className={"bg-red-400"}
+        onClick={handlePickWinner}
+      >
         Pick Winner
       </Button>
     </div>
